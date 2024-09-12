9fa73d1a27f686dfd102ecb3d46b789cfc831b9a
sana ullah <sunnykhansadda@gmail.com>
2024-09-12 19:36:53 +0500

    changes

--- src/components/PlanTravel.js
+++ src/components/PlanTravel.js
@@ -12,16 +12,16 @@ const PlanTravel = () => {
         <div className="absolute top-1/2 -translate-y-1/2 2xl:pl-20 lg:pl-10 md:pl-6 px-4">
           <h3
             className="2xl:text-[60px] lg:text-[50px] text-4xl   text-white font-bold font-Helvetica xl:max-w-[790px] w-full lg:leading-[70px] md:pb-0 pb-5 "
-            data-aos={window.innerWidth >= 600 ? "zoom-in-up" : null}
-            data-aos-duration={window.innerWidth >= 600 ? "1000" : null}
+            data-aos={window?.innerWidth >= 600 ? "zoom-in-up" : null}
+            data-aos-duration={window?.innerWidth >= 600 ? "1000" : null}
             //   data-aos-easing="ease-in-out-quad"
           >
             Plan your trip with a travel expert
           </h3>
           <p
             className="lg:text-4xl md:text-2xl text-base font-normal font-Helvetica  text-white lg:mt-6 md:mt-3 "
-            data-aos={window.innerWidth >= 600 ? "zoom-in-up" : null}
-            data-aos-duration={window.innerWidth >= 600 ? "1000" : null}
+            data-aos={window?.innerWidth >= 600 ? "zoom-in-up" : null}
+            data-aos-duration={window?.innerWidth >= 600 ? "1000" : null}
 
             //   data-aos-easing="ease-in-out-quad"
           >
--- src/components/sliders/SpecialOf_Slider.js
+++ src/components/sliders/SpecialOf_Slider.js
@@ -25,9 +25,11 @@ const SpecialOf_Slider = () => {
         <div
           key={offercard.id}
           className="flex sm:flex-row flex-col gap-5 py-6 px-4 bg-white cursor-pointer rounded-lg shadow-[0_4px_6px_rgba(0,0,0,0.1),0_1px_3px_rgba(0,0,0,0.08)] hover:shadow-xl transition-shadow duration-300 ease-in-out"
-          data-aos={window.innerWidth >= 600 ? "fade-up" : null}
-          data-aos-duration={window.innerWidth >= 600 ? "1000" : null}
-          data-aos-easing={window.innerWidth >= 600 ? "ease-in-out-quad" : null}
+          data-aos={window?.innerWidth >= 600 ? "fade-up" : null}
+          data-aos-duration={window?.innerWidth >= 600 ? "1000" : null}
+          data-aos-easing={
+            window?.innerWidth >= 600 ? "ease-in-out-quad" : null
+          }
         >
           <Image
             src={offercard.img}
